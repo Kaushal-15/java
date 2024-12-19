30301fb711d80c00d86ccb5ab63d5b48375e2f43
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-19 16:20:08 +0530

    file added

---
+++ data_types.java
@@ -0,0 +1,18 @@
+class Hello{
+    public static void main(String args[])
+    {
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = 'a';
+        String Name = "kaushal";
+        System.out.println("My number is: "+x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(symbol);
+        System.out.println(Name);
+
+    
+
+    }
+}
