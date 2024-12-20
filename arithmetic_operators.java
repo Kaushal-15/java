a899f7f1ce1258f3e6680855ea8897ca61328782
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-20 11:59:34 +0530

    file added

---
+++ arithmetic_operators.java
@@ -0,0 +1,22 @@
+public class arithmetic_operators {
+
+    public static void main(String[] args) {
+        //Implicit is happening below the conversion of smaller datatype to  larger one
+        int number1 = 5;
+        int number2 = 6;
+        //Addition 
+        System.out.println(number1 + number2);
+        //subtraction
+        System.out.println(number1 - number2);
+        //Multiplication
+        System.out.println(number1 * number2);
+        //division
+        System.out.println(number1 / number2);
+        //Modulo
+        System.out.println(number1 % number2);
+
+
+        
+        
+    }
+}
