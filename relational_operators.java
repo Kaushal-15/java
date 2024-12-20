155d8bace79387b5fd70971595d45c90fff42f5a
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-20 12:05:47 +0530

    file added

---
+++ relational_operators.java
@@ -0,0 +1,13 @@
+public class relational_operator {
+
+    public static void main(String[] args) {
+        int number1 = 12; int number2 = 15; 
+
+        System.out.println(number1 == number2);  // is not equal to 10 // 
+        System.out.println(number1 != number2); // is greater than 13 // 
+        System.out.println(number1> number2); // is less than 16 // 
+        System.out.println(number1 < number2);  // is greater than or equal to 19 // 
+        System.out.println(number1 >= number2);  //is less than or equal to 22 
+        System.out.println(number1 <= number2);
+    }
+}
