907a4c9ad769ad2e621e58cfea906828430be26b
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-22 13:52:28 +0530

    file added

---
+++ string2.java
@@ -0,0 +1,13 @@
+public class s {
+    public static void main(String[] args) {
+        String Name = "Kaushal";
+        System.out.println(Name.length());
+        System.out.println(Name.isEmpty());
+        //it returns false when something is there and true and nothing is there is the string
+
+        System.out.println(Name.toUpperCase());
+        System.out.println(Name.toLowerCase());
+        //in the above both lower and upper methods it does not change the original string
+    }
+
+}
