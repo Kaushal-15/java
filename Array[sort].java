c0b68657fd21cad80994393bdab3967764f6342e
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-25 22:04:47 +0530

    file added

---
+++ Array[sort].java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class s {
+    public static void main(String[] args) {
+        char vowels[] = {'e','u','a','o','i'};
+        Arrays.sort(vowels);
+        System.out.println(Arrays.toString(vowels));  
+         
+}
+
+}
