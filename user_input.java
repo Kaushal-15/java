bd03a25cd98bfbb1b25cca819eaf2b08dfa48be0
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-25 17:37:55 +0530

    file added

---
+++ user_input.java
@@ -0,0 +1,12 @@
+import  java.util.*;
+public class s {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name:");
+        String name =  input.nextLine();
+        System.out.printf("Hello %s .how are you.",name);
+        input.close();
+        
+    } 
+
+}
