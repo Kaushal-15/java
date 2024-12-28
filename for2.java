b36136cd5998557acef249aa25e2865d23e9afbb
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-28 14:21:39 +0530

    file added

---
+++ for2.java
@@ -0,0 +1,10 @@
+public class for2{
+    public static void main(String[] args) {
+       for (int i = 0; i <=10; i++) {
+            if(i%2!=0){
+           System.out.println(i);
+            }
+
+       }
+    }
+}
