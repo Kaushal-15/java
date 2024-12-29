ad6140aaa65259eb3c23cd4d0ea59229a65d180d
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-29 17:43:12 +0530

    file added

---
+++ random.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class random {
+    public static void main(String[] args) {
+      Random n1= new Random();
+      int n2=0;
+      while(n2!=5){
+        n2=n1.nextInt(10);
+        System.out.println(n2);
+      }
+       
+
+
+}
+
+}
