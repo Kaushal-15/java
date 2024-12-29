385a618942e3dbef874919fe7d94787a17cd1f14
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-29 16:57:08 +0530

    file added

---
+++ array1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class array1 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int[] num = new int[5];
+       num[0]=input.nextInt();
+       num[1]=input.nextInt();
+       num[2]=input.nextInt();
+       num[3]=input.nextInt();
+       num[4]=input.nextInt();
+       System.out.println(num[0]+num[1]+num[2]+num[3]+num[4]);
+       input.close();
+
+
+}
+
+}
