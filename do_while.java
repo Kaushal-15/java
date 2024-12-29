313bef0eae2f2f5eff01e7243a86974ef78510f5
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-29 18:01:35 +0530

    file added

---
+++ do_while.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class do_while {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+     
+       int count =0;
+       do { 
+           System.out.println("Enter the number should be greater than 10:");
+            count=input.nextInt();
+       } while (count<10);
+
+      
+
+
+}
+
+}
