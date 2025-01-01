e36c76a26267d8692d3236f82a0f079b1678c846
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-01 22:13:34 +0530

    file added

---
+++ function.java
@@ -0,0 +1,9 @@
+public class function{
+   void greeting(){
+    System.out.println("hello");
+   }
+    public static void main(String[] args) {
+        laptop obj1 = new laptop();
+        obj1.greeting();
+    }
+}
