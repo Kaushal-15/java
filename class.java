bf5d8c16a205685efb6d842f8806a41b147b130a
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-01 21:41:18 +0530

    file added

---
+++ class.java
@@ -0,0 +1,15 @@
+
+public class class {
+    //class declaration
+    int coffee = 20;
+    int tea = 15;
+    //should not declare object n the class 
+    public static void main(String[] args) {
+        // a class can have multiple objects in it
+        s server = new s();//object1
+        s server2 = new s();//object2
+        System.out.println(server.coffee);
+        System.out.println(server2.tea);
+}
+
+}
