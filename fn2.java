5450271e4619dc5c9fd5482d3e9e2c0e4801a13c
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-01 22:22:31 +0530

    file added

---
+++ fn2.java
@@ -0,0 +1,12 @@
+public class fn2{
+    int x =10;
+    int y =20;
+  void addons(){
+    
+    System.out.println(x+y);
+ }
+    public static void main(String[] args) {
+        fn2 sum = new fn2();
+        sum.addons();
+    }
+}
