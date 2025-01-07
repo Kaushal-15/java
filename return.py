dec37657b1c4016b60ac39a341196914e0c5dc94
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-07 22:30:23 +0530

    file added

---
+++ return.py
@@ -0,0 +1,14 @@
+
+public class return{
+  
+  int addons(int n1,int n2){
+  int sum = n1+n2;
+  return sum;
+   
+ }
+    public static void main(String[] args) {
+      return add = new return();
+        int Sum = add.addons(10,20);
+        System.out.println(Sum);
+    }
+}
