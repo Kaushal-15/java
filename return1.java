742a95a16cceab72783e6b0811a647c725d448ee
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-07 22:33:03 +0530

    file added

---
+++ return1.java
@@ -0,0 +1,14 @@
+
+public class return1{
+  
+  String get_name(String name){
+
+  return name;
+   
+ }
+    public static void main(String[] args) {
+        return1 Name = new return1();
+        String out = Name.get_name("kaushal");
+        System.out.println(out);
+    }
+}
