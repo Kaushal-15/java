82d7d681343b1cbd4bc47884c9f29f26bdf4287b
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-08 21:56:10 +0530

    Create for_each1.java

---
+++ for_each1.java
@@ -0,0 +1,9 @@
+public class for_each1{
+      public static void main(String[] args) {
+       String[] name = {"john","jack","jones"};
+       for(String var:name){
+        System.out.println(var);
+       }
+
+    }
+}
