2ee27a5d3c9d601db86e6b6146cbb5e8e950e44f
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-08 21:52:47 +0530

    for each loop for array

---
+++ for_each.java
@@ -0,0 +1,11 @@
+//for each loop or enhanced loop for using Arrays in easier way
+public class for_each{
+      public static void main(String[] args) {
+       int num[] = {1,2,3,4,5,6,7};
+//for each loop
+       for(int var:num){
+        System.out.println(var);
+       }
+
+    }
+}
